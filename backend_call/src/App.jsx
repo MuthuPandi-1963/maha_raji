22a7be99dcc45b612c9bf1b92e0fa4e326722483
MuthuPandi-1963 <muthupandir74738@gmail.com>
2025-05-30 12:17:50 +0530

    useEffect

--- backend_call/src/App.jsx
+++ backend_call/src/App.jsx
@@ -4,24 +4,36 @@ import { useEffect, useState } from "react";
 
 export default function App() {
   const [apiData, setApiData] = useState([])
+  const  [count,setCount] = useState(0)
+  
+  // useEffect(()=>{
+  //   fetchData()
+  // },[])
 
   useEffect(()=>{
     fetchData()
-  },[])
+    console.log("button clicked");
+    
+  },[count])
 
   const fetchData = async()=>{
     const response = await axios.get("https://dummyjson.com/posts")
     console.log(response.data.posts);
-    setApiData(response.data.posts)
+    const datas = response.data.posts
+    console.log(datas);
+    
+    setApiData(()=>datas.filter(val=>val.id <=count +1 ))
   }
   // fetchData()
-
+  console.log(count);
+  
   return (
     <>
+    <button onClick={()=>setCount(prev=>prev+1)}>Click</button>
     {
       apiData.map((val,id)=>(
         <>
-        <p>{val.views}</p>
+        <p>{val.title}   {val.views}</p>
         </>
       ))
     }
